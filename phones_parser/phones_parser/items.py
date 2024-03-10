f57ef0a8303bdb881801ba92caf658eebd7009e8
bulatsabirow <bulatsabirow@gmail.com>
2024-03-10 20:35:22 +0300

    = refactor spider and items code

--- phones_parser/phones_parser/items.py
+++ phones_parser/phones_parser/items.py
@@ -3,14 +3,20 @@
 # See documentation in:
 # https://docs.scrapy.org/en/latest/topics/items.html
 import re
-from operator import attrgetter, methodcaller
+from operator import methodcaller
+from typing import Optional
 
 import scrapy
 from itemloaders.processors import MapCompose, TakeFirst
-from w3lib.html import remove_tags
 
-def clean_data(raw_version: str) -> str:
-    return re.sub(r"\..*", "", raw_version).strip()
+
+def clean_data(raw_version: str) -> Optional[int]:
+    version = re.sub(r"([^\d.]+)|(\..*)", "", raw_version)
+    try:
+        return int(version)
+    except ValueError:
+        return
+
 
 class PhonesParserItem(scrapy.Item):
     # define the fields for your item here like:
--- phones_parser/phones_parser/spiders/ozon_phones.py
+++ phones_parser/phones_parser/spiders/ozon_phones.py
@@ -1,10 +1,9 @@
-import re
+from typing import Iterable
+from urllib.parse import urlencode
 
 import scrapy
-from urllib.parse import urlencode, urljoin
-
 from itemloaders import ItemLoader
-from scrapy.exceptions import CloseSpider
+from scrapy import Request
 
 from phones_parser.items import PhonesParserItem
 
@@ -12,42 +11,45 @@ from phones_parser.items import PhonesParserItem
 class OzonPhonesSpider(scrapy.Spider):
     name = "ozon_phones"
     allowed_domains = ["www.ozon.ru"]
-    start_urls = ["https://www.ozon.ru/category/smartfony-15502/?page=1&sorting=rating"]
+    start_url = "https://www.ozon.ru/category/smartfony-15502/"
 
     def __init__(self, *args, **kwargs):
         super(OzonPhonesSpider, self).__init__(*args, **kwargs)
         self.counter = 0
         self.page = 1
+        self.start_urls = [self.get_paginated_page()]
 
-    def paginate_page(self):
-        return "https://www.ozon.ru/category/smartfony-15502/?%s" % urlencode({"page": self.page, "sorting": "rating"})
+    def get_paginated_page(self):
+        return f'{self.start_url}?{urlencode({"page": self.page, "sorting": "rating"})}'
 
     def parse(self, response):
         links = response.css("div.widget-search-result-container.i7x > .xi7 > .vi6.v6i > .iv7 > a::attr(href)").getall()
-        print("parsed_url=", response.url, "phone_cards=", links)
+        print(f"{links=} ", len(links))
         for link in links:
             self.counter += 1
             if self.counter > 100:
                 break
-            print("!!!!", self.counter, link)
+
+            print("link = ", self.counter, link)
             yield response.follow(link, callback=self.parse_phone)
         else:
             self.page += 1
-            print("!!next!!=", self.paginate_page())
-            yield response.follow(self.paginate_page(), callback=self.parse)
+            print("page =", self.get_paginated_page())
+            yield response.follow(self.get_paginated_page(), callback=self.parse)
+
+    @staticmethod
+    def xpath_lookup_filter(value: str) -> str:
+        return f"//dt[span[contains(text(), '{value}')]]/following-sibling::dd//text()"
 
     def parse_phone(self, response):
-        print("start parse_phone ", response.url)
         loader = ItemLoader(item=PhonesParserItem(), selector=response)
-        os_xpath = response.xpath("//dt[span[contains(text(), 'Операционная система')]]/following-sibling::dd//text()").get().strip()
-        print("os_xpath=", os_xpath)
-        print("raw_version=", response.xpath("//dt[span[contains(text(), 'ерсия')]]/following-sibling::dd//text()").get())
-        version_xpath = re.sub(os_xpath, "", response.xpath("//dt[span[contains(text(), 'ерсия')]]/following-sibling::dd//text()").get())
-        print("version_xpath=", version_xpath)
+        os_xpath = self.xpath_lookup_filter('Операционная система')
+        version_xpath = self.xpath_lookup_filter('ерсия')
 
-        loader.add_value("os", os_xpath)
-        loader.add_value("version", version_xpath)
+        loader.add_xpath("os", os_xpath)
+        loader.add_xpath("version", version_xpath)
         loader.add_value("url", response.url)
+
         item = loader.load_item()
-        print("item=", item)
+        print("item = ", item)
         yield item
