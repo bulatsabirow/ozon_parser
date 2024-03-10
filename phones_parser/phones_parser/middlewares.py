67f88b7d0c0e664df06cc3a6c5231a618f4d346e
bulatsabirow <bulatsabirow@gmail.com>
2024-03-10 19:53:38 +0300

    = set page loading timeouts

--- phones_parser/phones_parser/middlewares.py
+++ phones_parser/phones_parser/middlewares.py
@@ -3,17 +3,18 @@
 # See documentation in:
 # https://docs.scrapy.org/en/latest/topics/spider-middleware.html
 import time
+from operator import eq
 from random import randint
-from time import sleep
 
 from scrapy import signals
-
 # useful for handling different item types with a single interface
-from itemadapter import is_item, ItemAdapter
 from scrapy.http import HtmlResponse
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.wait import WebDriverWait
 from webdriver_manager.chrome import ChromeDriverManager
 
 
@@ -68,25 +69,29 @@ class PhonesParserDownloaderMiddleware:
     # Not all methods need to be defined. If a method is not defined,
     # scrapy acts as if the downloader middleware does not modify the
     # passed objects.
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        options = Options()
-        options.add_experimental_option("excludeSwitches", ["enable-automation"])
-        options.add_experimental_option('useAutomationExtension', False)
-        options.add_argument("--disable-blink-features=AutomationControlled")
-        self.driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
-
-        self.driver.execute_cdp_cmd("Page.addScriptToEvaluateOnNewDocument", {
-            'source': """
+    BYPASS_ANTIBOT_CHALLENGE_JS_SCRIPT = """
                 delete window.cdc_adoQpoasnfa76pfcZLmcfl_Array;
                 delete window.cdc_adoQpoasnfa76pfcZLmcfl_JSON;
                 delete window.cdc_adoQpoasnfa76pfcZLmcfl_Promise;
                 delete window.cdc_adoQpoasnfa76pfcZLmcfl_Object;
                 delete window.cdc_adoQpoasnfa76pfcZLmcfl_Symbol;
                 delete window.cdc_adoQpoasnfa76pfcZLmcfl_Proxy;
-
             """
+    PAGE_LOADING_TIMEOUT = 15
+    ITEMS_PER_PAGE = 36
+    COMMON_TIMEOUT = 2
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        options = Options()
+        options.add_experimental_option("excludeSwitches", ["enable-automation"])
+        options.add_experimental_option('useAutomationExtension', False)
+        options.add_argument("--disable-blink-features=AutomationControlled")
+
+        self.driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+        self.driver.execute_cdp_cmd("Page.addScriptToEvaluateOnNewDocument", {
+            'source': self.BYPASS_ANTIBOT_CHALLENGE_JS_SCRIPT
         })
 
     @classmethod
@@ -107,8 +112,13 @@ class PhonesParserDownloaderMiddleware:
         # - or raise IgnoreRequest: process_exception() methods of
         #   installed downloader middleware will be called
         self.driver.get(request.url)
-        time.sleep(randint(4, 5))
-        print("!!request.url", request.url)
+
+        method = lambda driver: EC.presence_of_element_located((By.XPATH, "//dt[span[contains(text(), 'Операционная система')]]/following-sibling::dd"))
+        if request.url.startswith("https://www.ozon.ru/category/smartfony-15502/"):
+            method = lambda driver: len(driver.find_elements(By.CSS_SELECTOR, "div.widget-search-result-container.i7x > .xi7 > .vi6.v6i")) == self.ITEMS_PER_PAGE
+
+        WebDriverWait(self.driver, timeout=self.PAGE_LOADING_TIMEOUT).until(method)
+        time.sleep(self.COMMON_TIMEOUT)
         content = self.driver.page_source
         self.driver.delete_all_cookies()
         return HtmlResponse(
@@ -122,6 +132,7 @@ class PhonesParserDownloaderMiddleware:
         # - return a Response object
         # - return a Request object
         # - or raise IgnoreRequest
+        print("response", response)
         return response
 
     def process_exception(self, request, exception, spider):
